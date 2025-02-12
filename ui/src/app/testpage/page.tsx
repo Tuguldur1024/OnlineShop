17e2608ddfddeaf756af4a6266b1feefd9ac3ddd
Tuguldur1024 <purevochirbilguuntuguldur@gmail.com>
2025-02-12 23:22:46 +0800

    fix

--- ui/src/app/testpage/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import RecoverPassword from "@/components/RecoverPassword";
-import SecondStep from "@/components/BuySecondStep";
-
-const testPage = () => {
-  return <SecondStep />;
-};
-export default testPage;
